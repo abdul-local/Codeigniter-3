57ff762d77b637e2715c2b913b76ae4eecb01abc
abdul-local <abdullah.hamzan@gmail.com>
2020-08-22 16:25:23 +0800

    Membuat halaman Pertama dengan CodeIgniter

---
+++ application/views/percobaan.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ini halaman percobaan</title>
+</head>
+<body>
+    <h1>Ini halaman percobaan guyss</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta esse error quibusdam, in odio quae quaerat minima ipsam labore incidunt autem at accusamus nulla molestias quasi non. Officia, at consequuntur!</p>
+</body>
+</html>
